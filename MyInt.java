59b0ed7ed8d0b22a62474b651e4d6aac89d1b425
nshank3 <nshank3@uic.edu>
2016-03-14 20:35:16 -0500

    Update MyInt.java
    Added Division

--- MyInt.java
+++ MyInt.java
@@ -1,87 +1,81 @@
 import java.util.*;
 public class MyInt {
 	int[] MyArr;
+	String LargeInteger;
 	
-	public MyInt(int LargeAssNumber){		//int to array
-		String tmp = Integer.toString(LargeAssNumber);
-		MyArr = new int[tmp.length()];
-		for(int i=0; i<tmp.length(); i++){
-			MyArr[i] =tmp.charAt(i);
+	public MyInt(String LargeNumber){		//int to array
+		this.LargeInteger = new StringBuilder(LargeNumber).reverse().toString();
+		MyArr = new int[LargeInteger.length()];
+		for(int i=0; i<LargeInteger.length(); i++){
+			MyArr[i] = LargeInteger.charAt(i);
 		}
 	}
 
-	
-	public int ArrayToInt(){
-		int tmp =0;
-		for(int i=0; i<MyArr.length; i++){
-			tmp += MyArr[i]*Math.pow(10, MyArr.length-i);  //Multiply each number by decimal place and add to integer
-		}
-		
-		return tmp;
-	}
-	
+
 	
 	// Math Methods for MyInt
 	
 	
 	
 	public void AddToThis(MyInt MyArr2){
-		int tmp = this.ArrayToInt() + MyArr2.ArrayToInt();
-		String tmp2 = Integer.toString(tmp);
-		MyArr = new int[tmp2.length()];
-		for(int i=0; i<tmp2.length(); i++){
-			MyArr[i] = tmp2.charAt(i);
+		int i = 0;
+		while((i < MyArr.length) && (i<MyArr2.MyArr.length)){
+			MyArr[i] += MyArr2.MyArr[i];
+			if (MyArr[i] > 9){
+			MyArr[i] -=10;
+			MyArr[i+1] +=1;
+			}
+			//carry ten
+			i++;
 		}
-	
 	}
 	
 	public void SubtractFromThis(MyInt MyArr2){
-		int tmp = this.ArrayToInt() - MyArr2.ArrayToInt();
-		String tmp2 = Integer.toString(tmp);
-		MyArr = new int[tmp2.length()];
-		for(int i=0; i<tmp2.length(); i++){
-			MyArr[i] = tmp2.charAt(i);
+		int i = 0;
+		while((i < MyArr.length) && (i<MyArr2.MyArr.length)){
+			MyArr[i] -= MyArr2.MyArr[i];
+			if (MyArr[i] < 0){
+			MyArr[i] +=10;
+			MyArr[i+1] -=1;
+			}
+			//carry -ten
+			i++;
 		}
 	}
 		
 		public void MultiplyToThis(MyInt MyArr2){
-			int tmp = this.ArrayToInt()*MyArr2.ArrayToInt();
-			String tmp2 = Integer.toString(tmp);
-			MyArr = new int[tmp2.length()];
-			for(int i=0; i<tmp2.length(); i++){
-				MyArr[i] =tmp2.charAt(i);
-		}
+			int i = 0;
+			while((i < MyArr.length) && (i<MyArr2.MyArr.length)){
+				MyArr[i] *= MyArr2.MyArr[i];
+				if (MyArr[i] > 9){
+				
+				MyArr[i+1] += (MyArr[i]/10) % 10;
+				MyArr[i] = MyArr[i] %=10;
+				}
+				//carry -ten
+				i++;
+			}
 	}
-		public void DividetFromThis(MyInt MyArr2){
-			int tmp = this.ArrayToInt() / MyArr2.ArrayToInt();
-			String tmp2 = Integer.toString(tmp);
-			MyArr = new int[tmp2.length()];
-			for(int i=0; i<tmp2.length(); i++){
-				MyArr[i] = tmp2.charAt(i);
-		}
-	}	
+		public void DivideFromThis(MyInt Quotient){
+			int n = 0;
+			int k = 0;
+			int[] zero = new int[MyArr.length];
+			while (k<zero.length){
+			zero[k] = 0;
+			k++;
+			}
+			while(MyArr!=zero){
+				MyInt.this.SubtractFromThis(Quotient);
+				n++;
+			}
 		
-		public void ModuloFromThis(MyInt MyArr2){
-			int tmp = this.ArrayToInt() % MyArr2.ArrayToInt();
-			String tmp2 = Integer.toString(tmp);
-			MyArr = new int[tmp2.length()];
-			for(int i=0; i<tmp2.length(); i++){
-				MyArr[i] = tmp2.charAt(i);
-		}
+	String tmpstring = Integer.toString(n);
+	for(int i=0; i<tmpstring.length(); i++){
+		MyArr[i] = tmpstring.charAt(i);
 	}
+		}	
 		
-		public boolean IsGreaterThan(MyInt MyArr2){
-			return (this.ArrayToInt() > MyArr2.ArrayToInt());	
-		}
-		
-		public boolean IsLessThan(MyInt MyArr2){
-			return (this.ArrayToInt() < MyArr2.ArrayToInt());	
-		}
-		
-		public boolean IsEqualTo(MyInt MyArr2){
-			return (this.ArrayToInt() == MyArr2.ArrayToInt());	
-		}
-		
+
 	
 }
 
